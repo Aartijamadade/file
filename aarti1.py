35d2e6a328b5cff834847c9f20a58a0ddddc978d
Aartijamadade <126068558+Aartijamadade@users.noreply.github.com>
2023-03-31 19:39:43 +0530

    Update aarti1.py

--- aarti1.py
+++ aarti1.py
@@ -9,9 +9,8 @@ files = glob.glob(os.path.join(folder_path, '*'))
 
 files.sort(key=os.path.getmtime, reverse=True)
 
-# keep only the latest 5 files
+
 latest_files = files[:5]
 
-# delete all the other files
 for file in files[5:]:
     os.remove(file)
